9221d40ddbf0ab44d4dae0462016ae77b4201b82
Toshiaki Maki <tmaki@pivotal.io>
2018-07-15 01:15:19 +0900

    Bump to Kibana 6.3.1

--- add-blobs.sh
+++ add-blobs.sh
@@ -8,7 +8,7 @@ cd .downloads
 
 if [ ! -f ${DIR}/blobs/kibana/kibana-6.3.1-linux-x86_64.tar.gz ];then
     curl -L -O -J https://artifacts.elastic.co/downloads/kibana/kibana-6.3.1-linux-x86_64.tar.gz
-    bosh add-blob --dir=${DIR} kibana-6.2.1-linux-x86_64.tar.gz kibana/kibana-6.3.1-linux-x86_64.tar.gz
+    bosh add-blob --dir=${DIR} kibana-6.3.1-linux-x86_64.tar.gz kibana/kibana-6.3.1-linux-x86_64.tar.gz
 fi
 
 cd -
