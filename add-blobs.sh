7cae52c84c495bc0d8c3b8f6b3ef3d37e40a2416
Toshiaki Maki <tmaki@pivotal.io>
2018-07-15 01:08:29 +0900

    Bump to Kibana 6.3.1

--- add-blobs.sh
+++ add-blobs.sh
@@ -6,16 +6,9 @@ mkdir -p .downloads
 
 cd .downloads
 
-
-
-# if [ ! -f ${DIR}/blobs/java/openjdk-1.8.0_162.tar.gz ];then
-#     curl -L -O -J https://download.run.pivotal.io/openjdk-jdk/trusty/x86_64/openjdk-1.8.0_162.tar.gz
-#     bosh add-blob --dir=${DIR} openjdk-1.8.0_162.tar.gz java/openjdk-1.8.0_162.tar.gz
-# fi
-
-if [ ! -f ${DIR}/blobs/kibana/kibana-6.2.4-linux-x86_64.tar.gz ];then
-    curl -L -O -J https://artifacts.elastic.co/downloads/kibana/kibana-6.2.4-linux-x86_64.tar.gz
-    bosh add-blob --dir=${DIR} kibana-6.2.4-linux-x86_64.tar.gz kibana/kibana-6.2.4-linux-x86_64.tar.gz
+if [ ! -f ${DIR}/blobs/kibana/kibana-6.3.1-linux-x86_64.tar.gz ];then
+    curl -L -O -J https://artifacts.elastic.co/downloads/kibana/kibana-6.3.1-linux-x86_64.tar.gz
+    bosh add-blob --dir=${DIR} kibana-6.2.1-linux-x86_64.tar.gz kibana/kibana-6.3.1-linux-x86_64.tar.gz
 fi
 
 cd -
