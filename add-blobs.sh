79851e4dae1bc05a8d0ed535a3b5dd3a427d9011
Toshiaki Maki <tmaki@pivotal.io>
2018-08-30 11:17:40 +0900

    Bump to Kibana 6.4.0

--- add-blobs.sh
+++ add-blobs.sh
@@ -6,9 +6,11 @@ mkdir -p .downloads
 
 cd .downloads
 
-if [ ! -f ${DIR}/blobs/kibana/kibana-6.3.2-linux-x86_64.tar.gz ];then
-    curl -L -O -J https://artifacts.elastic.co/downloads/kibana/kibana-6.3.2-linux-x86_64.tar.gz
-    bosh add-blob --dir=${DIR} kibana-6.3.2-linux-x86_64.tar.gz kibana/kibana-6.3.2-linux-x86_64.tar.gz
+KIBANA_VERSION=6.4.0
+
+if [ ! -f ${DIR}/blobs/kibana/kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz ];then
+    curl -L -O -J https://artifacts.elastic.co/downloads/kibana/kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz
+    bosh add-blob --dir=${DIR} kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz kibana/kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz
 fi
 
 cd -
