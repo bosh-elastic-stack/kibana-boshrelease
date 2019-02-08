a2902e9a336d2a91dc7c4e40eb3765c2f68b248d
Toshiaki Maki <tmaki@pivotal.io>
2019-02-08 17:42:14 +0900

    Bump to Kibana 6.6.0

--- add-blobs.sh
+++ add-blobs.sh
@@ -6,7 +6,7 @@ mkdir -p .downloads
 
 cd .downloads
 
-KIBANA_VERSION=6.5.4
+KIBANA_VERSION=6.6.0
 
 if [ ! -f ${DIR}/blobs/kibana/kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz ];then
     curl -L -O -J https://artifacts.elastic.co/downloads/kibana/kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz
