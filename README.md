ac0f18b5cdde23e7f73d77525b6d9abd33f1c9f5
Toshiaki Maki <tmaki@pivotal.io>
2019-02-22 01:29:17 +0900

    Add kibana.config_options property

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 bosh create-release --name=kibana --force --timestamp-version --tarball=/tmp/kibana-boshrelease.tgz && bosh upload-release /tmp/kibana-boshrelease.tgz  && bosh -n -d kibana deploy manifest/kibana.yml --no-redact
 ```
 
+### How to test spec files
+
+```
+bundle install
+bundle exec rspec spec/jobs/*_spec.rb
+```
---
+++ spec/jobs/kibana_spec.rb
@@ -0,0 +1,63 @@
+require 'rspec'
+require 'yaml'
+require 'bosh/template/test'
+require 'base64'
+
+describe 'kibana job' do
+  let(:release) { Bosh::Template::Test::ReleaseDir.new(File.join(File.dirname(__FILE__), '../..')) }
+  let(:job) { release.job('kibana') }
+
+  describe 'kibana.yml' do
+    let(:template) { job.template('config/kibana.yml') }
+    let(:links) { [
+        Bosh::Template::Test::Link.new(
+          name: 'elasticsearch',
+          instances: [Bosh::Template::Test::LinkInstance.new(address: '10.0.8.2')],
+          properties: {
+            'elasticsearch'=> {
+              'cluster_name' => 'test'
+            },
+          }
+        )
+      ] }
+
+    it 'configures defaults successfully' do
+      config = YAML.safe_load(template.render({}, consumes: links))
+      expect(config['server.port']).to eq(5601)
+      expect(config['server.host']).to eq('0.0.0.0')
+      expect(config['kibana.index']).to eq('.kibana')
+      expect(config['kibana.defaultAppId']).to eq('discover')
+      expect(config['elasticsearch.url']).to eq('http://10.0.8.2:9200')
+      expect(config['elasticsearch.requestTimeout']).to eq(300000)
+      expect(config['elasticsearch.shardTimeout']).to eq(30000)
+    end
+
+    it 'makes a request to ES secure' do
+      config = YAML.safe_load(template.render({'kibana' => {
+        'elasticsearch' => {
+          'protocol' => 'https',
+          'port' => '443',
+          'username' => 'admin',
+          'password' => 'password',
+        }
+      }}, consumes: links))
+      expect(config['elasticsearch.url']).to eq('https://10.0.8.2:443')
+      expect(config['elasticsearch.customHeaders']['Authorization']).to eq('Basic YWRtaW46cGFzc3dvcmQ=')
+      expect(config['elasticsearch.requestHeadersWhitelist']).to eq([])
+    end
+
+    it 'configures kibana.config_options' do
+      config = YAML.safe_load(template.render({'kibana' => {
+        'config_options' => {
+            'xpack' => {
+              'security' => {
+                'enabled' => true
+              }
+          }
+        }
+      }}, consumes: links))
+      puts YAML.dump(config)
+      expect(config['xpack']['security']['enabled']).to eq(true)
+    end
+  end
+end
